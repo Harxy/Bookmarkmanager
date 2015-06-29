a743c3678440b1fc12b236924c4c87ba789d58aa
Paul Harker <harkerp@gmail.com>
2015-06-29 15:32:30 +0100

    stuff

--- lib/bookmark_manager.rb
+++ lib/bookmark_manager.rb
@@ -7,7 +7,19 @@ class BookmarkManager < Sinatra::Base
 
     get '/links' do
      @links = Link.all?
+
      erb :'links/index'
+    end
+
+    post '/links' do
+      Link.create(url: params[:url], title: params[:title])
+      redirect to('/links')
+    end
+
+    get '/links/new' do
+
+      erb :links_new
+
     end
 
     run! if app_file == $0
